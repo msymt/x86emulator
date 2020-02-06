1313311281055cab7215d9ecdb1ca0435de263df
msy <nam.nam580@gmail.com>
2020-02-06 23:14:14 +0900

    [add] ヘッダファイルを追加

---
+++ emulator/emulator.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// メモリ: 1MB
+#define MEMORY_SIZE (1024 * 1024);
+
+typedef struct {
+  // 汎用レジスタ
+  uint32_t registers[REGISTERS_COUNT];
+
+  // ELFAGSレジスタ
+  uint32_t eflags;
+
+  // メモリ(バイト列)
+  uint8_t memory;
+
+  // プログラムカウンタ
+  uint32_t eip;
+} Emulator;
+
+Emulator* create_emu(size_t size, uint32_t eip, uint32_t esp);
+
+void destroy_emu(Emulator* emu);
+
+void mov_r32_imm32(Emulator* emu);
+
+void short_jump(Emulator* emu);
